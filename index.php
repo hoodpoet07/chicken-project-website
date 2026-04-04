7c7a285b28b740d955c9a5269f59af3c1c9fd8ab
Hamamunashe Tirekerwi <hamamunashetirekerwi@gmail.com>
2026-04-05 01:06:32 +0200

    adding icons to the contact section

--- index.php
+++ index.php
@@ -4,22 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JTK Farm</title>
-    <link rel="icon" type="image/png" href="images/image.png" />
+    <link rel="icon" type="image/png" href="icons/image.png" />
     <link rel="stylesheet" href="style.css">
 </head>
 <body>    
     <div class="navbar">
         <h1>JTK Farm</h1>
-        <h2>Home</h2>
         <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="about.php">About</a></li>
-            <li><a href="contact.php">Contact</a></li>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
         </ul>
     </div>
     <section id="home" class="section">Home Content</section>
     <section id="about" class="section">About Content</section>
-    <section id="contact" class="section">Contact Content</section>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore ullam vel commodi, explicabo natus iste laborum eveniet eligendi voluptas voluptatum odio repellat perspiciatis molestias mollitia nesciunt, veniam eum quod at?</p>
+    <section id="contact" class="section">
+        <a href="http://www.facebook.com/Stanley Junior Tapiwanashe Tirekerwi" target="_blank"><img src="icons/icons8-facebook-50.png" alt="Facebook">Visit Us on FAcebook</a>
+        <a href="http://wa.me/263780644286" target="_blank"><img src="icons\icons8-whatsapp-50.png" alt="WhatsApp">Chat with us on Whatsapp</a>
+        <a href="http://www.x.com/tapiwanashetirekerwi" target="_blank"><img src="icons\icons8-x-50.png" alt="Twitter">Follow us on Twitter</a>
+        <a href="http://www.instagram.com/juniortk" target="_blank"><img src="icons\icons8-instagram-50.png" alt="Instagram">Follow us on Instagram</a>
+        <a href="http://www.linkedin.com/in/tapiwanashetirekerwi" target="_blank"><img src="icons\icons8-linkedin-2-50.png" alt="LinkedIn">Connect with us on LinkedIn</a>
+    </section>
 </body>
 </html>
